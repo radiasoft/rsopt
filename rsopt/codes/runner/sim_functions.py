d2dec131367ac0a6581f7839c808e315e4c47aae
cchall <cchall@users.noreply.github.com>
2020-06-07 18:07:21 +0000

    adding option to set expected persistant workers

--- rsopt/codes/runner/sim_functions.py
+++ rsopt/codes/runner/sim_functions.py
@@ -1,20 +1,31 @@
 from rsopt.codes.runner.Runner import Runner
 from libensemble.message_numbers import WORKER_DONE, WORKER_KILL, TASK_FAILED
-import os
+import os, logging
 import numpy as np
 from time import sleep
 
 
 def sim_function_with_runner(H, persis_info, sim_specs, libE_info):
-    print('sim launching from {}'.format(os.getcwd()))
+    logger = logging.getLogger('libensemble')
+    logger.info('sim launching from {}'.format(os.getcwd()))
     
-    # Worker 1 for libEnsemble does not do sim evals but we are farming out to servers based on worker number
+    # Worker 1 for libEnsemble (with APOSMM Generator) does not do sim evals 
+    # but we are farming out to servers based on worker number
     # You should use nworkers = number_of_servers + 1 then workerID - 1 is [1, number_of_servers]
     # and we can assign to server
-    server_id = libE_info['workerID'] - 1
-    print('Server id is: {}'.format(server_id))
+    
+    try:
+        persistant_worker_count = sim_specs['user']['persistant_worker_count']
+    except KeyError:
+        logger.warning("persistant_worker_count was not explicitly set. Assuming 1 persistant worker.")
+        persistant_worker_count = 1
+        
+    server_id = libE_info['workerID'] - persistant_worker_count
+    # Sleep time is used to make sure we do not start rsmpi jobs too closely together
+    # past experience has shown this leads to fatal errors. Possibly due to temporary files that are created in the same location?
     st = server_id * 5.
     sleep(st)
+    
     x = H['x'][0]
     base_schema = sim_specs['user']['base_schema']
     objective_function = sim_specs['user']['objective_function']
