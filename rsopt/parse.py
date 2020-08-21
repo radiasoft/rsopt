cb14107d77d9b1daf232a8432624205ed70ac3f9
chall <chall@radiasoft.net>
2020-08-21 17:48:51 -0600

    Developing parser

---
+++ rsopt/parse.py
@@ -0,0 +1,3 @@
+# Handle reading of YAML template files. Should just read in data and put it corresponding object.
+# Will supplant some of the functionality held by Runner currently
+# Parameter and Setting object are defined externally
