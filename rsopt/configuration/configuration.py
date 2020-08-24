b3896ab94e467e3289c917d6d7186a0332e60dd2
cchall <chall@radiasoft.net>
2020-08-23 18:20:06 -0600

    New functionality for Configuration

--- rsopt/configuration/configuration.py
+++ rsopt/configuration/configuration.py
@@ -6,10 +6,17 @@ class Configuration:
     def set_jobs(self, jobs):
         self.jobs.extend(jobs)
 
-    def get_formatted_parameters_list(self, attribute, formatter=list):
+    def get_dimension(self):
+        dim = 0
+        for job in self.jobs:
+            dim += len(job.parameters)
+
+        return dim
+
+    def get_parameters_list(self, attribute, formatter=list):
         # get list attribute from all job parameters and return based on formatter
         attribute_list = []
         for job in self.jobs:
-            attribute_list.extend(*job.__getattribute__(attribute))
+            attribute_list.extend(job._parameters.__getattribute__(attribute)())
 
-        return formatter(*attribute_list)
+        return formatter(attribute_list)
