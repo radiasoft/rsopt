26eb2978b566efe11cd22465cb44950f89755770
cchall <chall@radiasoft.net>
2020-10-26 10:55:51 -0600

    Don't try to copy input file if using Python function imported in run script

--- rsopt/libe_tools/optimizer.py
+++ rsopt/libe_tools/optimizer.py
@@ -164,7 +164,9 @@ class libEnsembleOptimizer(Optimizer):
             if job.code in _USE_WORKER_DIRS_DEFAULT:
                 self.libE_specs.setdefault('use_worker_dirs', True)
                 self.libE_specs.setdefault('sim_dirs_make', True)
-                if job.code == 'python':
+                if job.code == 'python' and job.setup.get('input_file'):
+                    # If an input file is registered then copy to run dir, otherwise expect Python function defined or
+                    # imported into input script
                     self.libE_specs.setdefault('sim_dir_symlink_files', [job.setup['input_file'],])
                 break
 
