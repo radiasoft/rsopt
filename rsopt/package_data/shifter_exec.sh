0a015bff6e0d8b9f9359f7cbd228267098f87749
cchall <chall@radiasoft.net>
2020-11-04 20:48:16 -0800

    adding enviroment settings for genesis

--- rsopt/package_data/shifter_exec.sh
+++ rsopt/package_data/shifter_exec.sh
@@ -1,6 +1,9 @@
 ulimit -c 0
 unset PYTHONPATH
 unset PYTHONSTARTUP
+unset LESSCLOSE 
+unset LESSOPEN
+unset LESS
 export PYENV_ROOT=/home/vagrant/.pyenv
 export HOME=/home/vagrant
 source /home/vagrant/.bashrc >& /dev/null
