f358061ae29876a16937a2a4587040785158e3d0
cchall <chall@radiasoft.net>
2021-05-10 09:04:43 -0700

    Make sure pack does not try to include standard library imports or you will get NoneTypes in file list

--- rsopt/pkcli/pack.py
+++ rsopt/pkcli/pack.py
@@ -11,7 +11,8 @@ def _get_local_modules(script_name):
     finder = modulefinder.ModuleFinder(path=['.', ])
     finder.run_script(script_name)
     for name, mod in finder.modules.items():
-        file_list.append(mod.__file__)
+        if mod.__file__:  # Skips standard library imports
+            file_list.append(mod.__file__)
 
     return file_list
 
@@ -78,6 +79,7 @@ def _create_tar(name, file_list):
     tarname = f'{name}.tar.gz'
     tar = tarfile.open(tarname, 'w:gz')
     for file in file_list:
+        print("Adding file to tarball: {}".format(file))
         tar.add(file)
 
     tar.close()
