aa55d8362e2f934d9ee3c26dc9cfc2665641a6d1
chall <chall@radiasoft.net>
2021-05-17 16:33:15 -0600

    Swapping remaining examples to be used in testing to be in package_data Updating test file

---
+++ rsopt/package_data/examples/preprocess.py
@@ -0,0 +1,5 @@
+
+def f_pre(J):
+    # Example pre processing to modify the number of particles used
+    particles = J['inputs']['bunched_beam.n_particles_per_bunch']
+    J['inputs']['bunched_beam.n_particles_per_bunch'] = particles // 4
