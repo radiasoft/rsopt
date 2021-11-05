f07f83a1caaea5e9182dac479622a3d34a92569a
chall <chall@radiasoft.net>
2021-11-05 14:25:12 -0600

    Updating aposmm

--- rsopt/libe_tools/optimizer_aposmm.py
+++ rsopt/libe_tools/optimizer_aposmm.py
@@ -61,8 +61,8 @@ class AposmmOptimizer(optimizer.libEnsembleOptimizer):
         for key, val in self._options.items():
             user_keys[key] = val
         self.gen_specs.update({'gen_f': aposmm,
-                               # TODO: This will need to be updated if other options beyond nlopt are added (
-                               'in': ['x', 'f', 'local_pt', 'sim_id', 'returned', 'x_on_cube', 'local_min'],
+                               'persis_in': self._set_persis_in(software, method) +
+                                            [n[0] for n in gen_out],
                                'out': gen_out,
                                'user': user_keys})
 
@@ -75,3 +75,12 @@ class AposmmOptimizer(optimizer.libEnsembleOptimizer):
     def _configure_specs(self):
         self.nworkers = self._config.options.nworkers
         super(AposmmOptimizer, self)._configure_specs()
+
+    def _set_persis_in(self, software, method):
+        # method name should be returned from get_local_optimizer_method
+        # only sets the unique (to the method) portion of the persis_in fiel
+        s = self._OPT_SCHEMA[software]
+        m = s['methods'][method]
+        assert m['aposmm_support'], f'Method {m} from software {s} is not supported by APOSMM'
+
+        return m['persis_in']
