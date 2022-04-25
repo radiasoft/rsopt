15bd3c9ea4622a137d459241a6b6e3fc01e38f0e
chall <chall@radiasoft.net>
2022-04-24 20:42:08 -0600

    Initial working version

--- rsopt/configuration/jobs.py
+++ rsopt/configuration/jobs.py
@@ -115,6 +115,14 @@ class Job:
             return True
         return False
 
+    @property
+    def ignored_files(self) -> list:
+        # Get files that should be ignore by sirepo.lib parse
+        ignored_files = self._setup.get_ignored_files
+        ignored_files.append(self.input_distribution)
+
+        return ignored_files
+
     @parameters.setter
     def parameters(self, parameters):
         reader = get_reader(parameters, 'parameters')
@@ -161,4 +169,5 @@ class Job:
         # Import input_file
         if self._setup.setup.get('input_file'):
             self._setup.input_file_model = self._setup.parse_input_file(self._setup.setup.get('input_file'),
-                                                                        self.setup.get('execution_type', False) == 'shifter')
+                                                                        self.setup.get('execution_type', False) == 'shifter',
+                                                                        self.ignored_files)
