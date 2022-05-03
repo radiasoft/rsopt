d2154b6aee70c2d5a8aa86610643497f4324287a
chall <chall@radiasoft.net>
2022-05-02 19:00:20 -0600

    Initial fix

--- rsopt/pkcli/pack.py
+++ rsopt/pkcli/pack.py
@@ -2,8 +2,10 @@ import rsopt.parse as parse
 import sirepo.sim_data  # Can't run import sirepo and call sirepo.sim_data. ??
 import modulefinder
 import tarfile
+import pathlib
 import os
 from sirepo.template import lattice
+
 
 def _get_local_modules(script_name):
     # return a list of modules used defined in the same directory as `script_name` (including `script_name`)
@@ -16,24 +18,28 @@ def _get_local_modules(script_name):
 
     return file_list
 
+
 def _get_processing(job):
     # return pre/post processing scripts and local dependencies (if any)
     file_list = []
     for attr in ['pre_process', 'post_process']:
-        if job.__getattribute__(attr):
-            file_name = job.__getattribute__(attr)[0]
+        # TODO: might want to just make a getter for setup.setup - also used in _get_files_from_job
+        if job._setup.setup.get(attr):
+            file_name = job._setup.setup.get(attr)[0]
             files = _get_local_modules(file_name)
             file_list.extend(files)
 
     return file_list
 
 
-def _get_file_list_from_model(model, code_name):
+def _get_file_list_from_model(model, code_name, input_file_path):
+    # Use same path as configuration file when packing
+    model_path = pathlib.Path(input_file_path).parents[0]
     # Get input and lattice if exists
-    file_list = [model['_SimData__source'].basename, ]
+    file_list = [model_path.joinpath(model['_SimData__source'].basename), ]
     try:
-        lattice_file = model._SimData__adapter._lattice_path('', model)
-        file_list.append(lattice_file)
+        lattice_file = model._SimData__adapter._lattice_path('', model).basename
+        file_list.append(model_path.joinpath(lattice_file))
     except AttributeError:
         pass
 
@@ -41,9 +47,10 @@ def _get_file_list_from_model(model, code_name):
     _sim_data, _, _schema = sirepo.sim_data.template_globals(code_name)
     files = lattice.LatticeUtil(model, _schema).iterate_models(
         lattice.InputFileIterator(_sim_data, update_filenames=False),).result
-    file_list.extend(files)
+    file_list.extend([model_path.joinpath(ff) for ff in files])
 
     return file_list
+
 
 def _get_files_from_job(job):
     # return all files defined in a job
@@ -56,13 +63,15 @@ def _get_files_from_job(job):
         file_list.extend(files)
     else:
         m = job._setup.input_file_model
-        files = _get_file_list_from_model(m, job.code)
+        # TODO: might want to just make a getter for setup.setup
+        files = _get_file_list_from_model(m, job.code, job._setup.setup['input_file'])
         file_list.extend(files)
 
     files = _get_processing(job)
     file_list.extend(files)
 
     return file_list
+
 
 def _get_files_from_options(config):
     # return all files defined in options
@@ -75,6 +84,7 @@ def _get_files_from_options(config):
 
     return file_list
 
+
 def _create_tar(name, file_list):
     tarname = f'{name}.tar.gz'
     tar = tarfile.open(tarname, 'w:gz')
@@ -86,8 +96,9 @@ def _create_tar(name, file_list):
 
     return tarname
 
+
 def configuration(config, ignore=None, add=None):
-    file_list = [config,]
+    file_list = [config, ]
     config_yaml = parse.read_configuration_file(config)
     _config = parse.parse_yaml_configuration(config_yaml)
 
@@ -114,4 +125,3 @@ def configuration(config, ignore=None, add=None):
     tar_name = os.path.splitext(config)[0]
     create_filename = _create_tar(tar_name, file_list)
     print(f"Created tarball: {create_filename}")
-
