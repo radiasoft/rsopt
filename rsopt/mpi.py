38ca43d8ef82d9f4dcaabcc6bee607af95b848c1
Joshua Einstein-Curtis <joshec@radiasoft.net>
2022-07-18 11:52:05 -0400

    Adding same change to mpi.py per request

--- rsopt/mpi.py
+++ rsopt/mpi.py
@@ -1,7 +1,8 @@
 
 def get_mpi_environment():
     try:
-        from mpi4py import MPI
+        import mpi4py
+        mpi4py.rc.initialize = False
     except ModuleNotFoundError:
         # mpi4py not installed so it can't be used
         return
@@ -11,6 +12,8 @@ def get_mpi_environment():
         # (if user did start MPI with size 1 this would be an illegal configuration since: main + 1 worker = 2 ranks)
         return
 
+    from mpi4py import MPI
+    MPI.Init()
     nworkers = MPI.COMM_WORLD.Get_size() - 1
     is_manager = MPI.COMM_WORLD.Get_rank() == 0
     mpi_environment = {'mpi_comm': MPI.COMM_WORLD, 'comms': 'mpi', 'nworkers': nworkers, 'is_manager': is_manager}
