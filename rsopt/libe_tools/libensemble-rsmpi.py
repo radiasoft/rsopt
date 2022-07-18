c8b05ec136c6f8abf09eabc2d8136080fa69847c
cchall <chall@radiasoft.net>
2022-07-18 08:33:35 -0600

    Fixes #90 Fixes #100

--- rsopt/libe_tools/libensemble-rsmpi.py
+++ rsopt/libe_tools/libensemble-rsmpi.py
@@ -2,16 +2,17 @@
 import argparse
 import subprocess
 import sys
+import time
 
-run_string = 'rsmpi -n {n} -h {h} {args}'
+run_string = 'rsmpi -n {n} -h {h} -t {t} {args}'
 
 parser = argparse.ArgumentParser(description="Wrap rsmpi for use by libensemble. Converts required MPICH flags"
                                              "to rsmpi flags.")
 
 parser.add_argument("-np", dest="n", required=True,
                     help="Pass number of processors to rsmpi")
-parser.add_argument("--ppn", dest="ppn", required=False,
-                    help="Not required by rsmpi, but supplied by libEnsemble. Input is not used if given.")
+parser.add_argument("--ppn", dest="t", required=False,
+                    help="Number of processes per node to use")
 parser.add_argument('args', nargs=argparse.REMAINDER, help="All other arguments are appended after rsmpi")
 server_spec = parser.add_mutually_exclusive_group(required=True)
 server_spec.add_argument("-machinefile", dest="machinefile",
@@ -27,6 +28,17 @@ def get_host_from_machinefile(machinefile):
     return host
 
 
+def stream_process(process):
+    go = process.poll() is None
+    for line in process.stdout:
+        sys.stdout.write(line.decode())
+        sys.stdout.flush()
+    for line in process.stderr:
+        sys.stderr.write(line.decode())
+        sys.stderr.flush()
+    return go
+
+
 parsed_args = parser.parse_args()
 if parsed_args.machinefile is not None:
     h = get_host_from_machinefile(parsed_args.machinefile)
@@ -34,14 +46,17 @@ else:
     h = parsed_args.hosts
 args = ' '.join(parsed_args.args)
 
-formatted_run_string = run_string.format(n=parsed_args.n, h=h, args=args).split()
+formatted_run_string = run_string.format(n=parsed_args.n, h=h, t=parsed_args.t, args=args).split()
 
 # Execute
 try:
     run_status = subprocess.Popen(formatted_run_string, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-    out, err = run_status.communicate()
+    while stream_process(run_status):
+        time.sleep(0.001)
 
+    # Handle any final write
+    out, err = run_status.communicate()
     if out:
         sys.stdout.write(out.decode())
         sys.stdout.flush()
