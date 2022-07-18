92a2c22ea9e06daba921cc1be26662e22105ab8f
Joshua Einstein-Curtis <joshec@radiasoft.net>
2022-07-18 16:33:08 -0400

    Fixing bug pointed out by @cchall

--- rsopt/mpi.py
+++ rsopt/mpi.py
@@ -7,13 +7,14 @@ def get_mpi_environment():
         # mpi4py not installed so it can't be used
         return
 
+    from mpi4py import MPI
+    MPI.Init()
+
     if not MPI.COMM_WORLD.Get_size() - 1:
         # MPI not being used
         # (if user did start MPI with size 1 this would be an illegal configuration since: main + 1 worker = 2 ranks)
         return
 
-    from mpi4py import MPI
-    MPI.Init()
     nworkers = MPI.COMM_WORLD.Get_size() - 1
     is_manager = MPI.COMM_WORLD.Get_rank() == 0
     mpi_environment = {'mpi_comm': MPI.COMM_WORLD, 'comms': 'mpi', 'nworkers': nworkers, 'is_manager': is_manager}
