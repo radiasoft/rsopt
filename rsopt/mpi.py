d698629d8da41da9f1ef6670c3dd629722f15b9a
Joshua Einstein-Curtis <joshec@radiasoft.net>
2022-07-19 12:13:55 -0400

    Updating with subprocess-based MPI checking

--- rsopt/mpi.py
+++ rsopt/mpi.py
@@ -1,19 +1,38 @@
-
 def get_mpi_environment():
+    # Test for mpi4py install
     try:
         import mpi4py
         mpi4py.rc.initialize = False
+        from mpi4py import MPI
     except ModuleNotFoundError:
         # mpi4py not installed so it can't be used
-        return
+        return None
 
-    from mpi4py import MPI
-    MPI.Init()
+    from inspect import currentframe, getframeinfo
+    frameinfo = getframeinfo(currentframe())
+    print(f"Initializing MPI from {frameinfo.filename}:L{frameinfo.lineno}", flush=True)
+
+    #import faulthandler
+    #import sys
+    #faulthandler.enable(file=sys.stderr, all_threads=True)
+
+    # Test MPI intialization in another thread
+    import subprocess
+    import os
+    import rsopt
+    fname = os.path.dirname(rsopt.__file__) + "/__main__.py"
+    pp = subprocess.run(["python", fname])
+    
+    if pp.returncode != 0:
+        return None
+
+    # Should already be initialized
+    # MPI.Init()
 
     if not MPI.COMM_WORLD.Get_size() - 1:
         # MPI not being used
         # (if user did start MPI with size 1 this would be an illegal configuration since: main + 1 worker = 2 ranks)
-        return
+        return None
 
     nworkers = MPI.COMM_WORLD.Get_size() - 1
     is_manager = MPI.COMM_WORLD.Get_rank() == 0
