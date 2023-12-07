8892435b5526efba7533a5118eaf7048dbd029a3
cchall <chall@radiasoft.net>
2023-12-07 15:26:26 -0500

    opal tests for parser

--- tests/test_models.py
+++ tests/test_models.py
@@ -5,13 +5,13 @@ from rsopt.configuration.setup.opal import Opal
 
 _TMP_DIR = 'tmp'
 
-# TODO: New set of tests
-#  Test case (mixed, caps, lower)
+# NOTE: Would be a good candidate to re-write in pytest to leverage parametrize
+
 #  bad commands are rejected
 #  bad command fields are regjected
 #  bad elements are rejected
 #  bad element parameters are rejected
-#  Test bunched_beam.Po since it is case sensitive?
+
 
 class TestElegantModels(unittest.TestCase):
 
@@ -25,7 +25,6 @@ class TestElegantModels(unittest.TestCase):
     def test_elegant_case_passing(self):
         # Various mixtures of cases that should be valid
         # Element Names and Parameters can be entered with any combination of case
-        # Command values are case-sensitive
         setup = Elegant()
         setup.setup['input_file'] = 'e.ele'
         setup.input_file_model = Elegant.parse_input_file('support/linac_files/first.ele', shifter=False)
@@ -57,17 +56,17 @@ class TestElegantModels(unittest.TestCase):
         self.assertRaisesRegex(ValueError, 'momentum_aperture was not found',
                                setup._edit_input_file_schema, kwarg_dict)
 
-    def elegant_element_name_failure(self):
+    def test_elegant_element_name_failure(self):
         setup = Elegant()
         setup.setup['input_file'] = 'e.ele'
         setup.input_file_model = Elegant.parse_input_file('support/linac_files/first.ele', shifter=False)
 
         kwarg_dict = {
-            'bunched_beam.po': 600.,  # Must be 'Po'
+            'not_an_element.k1': 600.,  # Must be 'Po'
         }
 
-        new_model = setup._edit_input_file_schema(kwarg_dict=kwarg_dict)
-        new_model.write_files(self.test_dir.name)
+        self.assertRaisesRegex(ValueError, 'not_an_element was not found',
+                               setup._edit_input_file_schema, kwarg_dict)
 
     def tearDown(self):
         self.test_dir.cleanup()
@@ -87,21 +86,70 @@ class TestOpalModels(unittest.TestCase):
         # Various mixtures of cases that should be valid
         # Element Names and Parameters can be entered with any combination of case
         # Command values are case-sensitive
-        setup = Elegant()
+        setup = Opal()
         setup.setup['input_file'] = 'e.in'
         setup.input_file_model = Opal.parse_input_file(self.input_file, shifter=False)
 
         kwarg_dict = {
             'distribution.1.nbin': 12,
-            # 'load_parameters.1.allow_missing_parameters': 0,
-            # 'bunched_beam.Po': 600.,  # Correct case for case-sensitive name
-            # 'TUBe.X_mAX': 42,  # Element names and parameters are never case sensitive for rsopt
-            # 'correction_matrix_output.BnL_Units': 1  # Incorrect case for case-sensitive name (is BnL_units)
-
+            'FIELDSOLVER.parFFTx': 'true',
+            'Q_107.K1S': 50.,  # Correct case for case-sensitive name
+            'cc2.fReq': 1e9,  # Correct case for case-sensitive name
         }
 
         new_model = setup._edit_input_file_schema(kwarg_dict=kwarg_dict)
         new_model.write_files(self.test_dir.name)
+
+    def test_opal_command_name_failure(self):
+        # Command not in .in, should not be found
+        setup = Opal()
+        setup.setup['input_file'] = 'e.in'
+        setup.input_file_model = Opal.parse_input_file(self.input_file, shifter=False)
+
+        kwarg_dict = {
+            'geometry.length': 5.0,
+        }
+
+        # new_model = setup._edit_input_file_schema(kwarg_dict=kwarg_dict)
+
+        self.assertRaisesRegex(ValueError, 'geometry was not found',
+                               setup._edit_input_file_schema, kwarg_dict)
+
+    def elegant_opal_name_failure(self):
+        setup = Opal()
+        setup.setup['input_file'] = 'e.in'
+        setup.input_file_model = Opal.parse_input_file(self.input_file, shifter=False)
+
+        kwarg_dict = {
+            'bunched_beam.po': 600.,
+        }
+
+        new_model = setup._edit_input_file_schema(kwarg_dict=kwarg_dict)
+        new_model.write_files(self.test_dir.name)
+
+    def test_opal_element_name_failure(self):
+        setup = Opal()
+        setup.setup['input_file'] = 'e.in'
+        setup.input_file_model = Opal.parse_input_file(self.input_file, shifter=False)
+
+        kwarg_dict = {
+            'not_an_element.k1': 600.,
+        }
+
+        self.assertRaisesRegex(ValueError, 'not_an_element was not found',
+                               setup._edit_input_file_schema, kwarg_dict)
+
+    def test_opal_element_parameter_failure(self):
+        setup = Opal()
+        setup.setup['input_file'] = 'e.in'
+        setup.input_file_model = Opal.parse_input_file(self.input_file, shifter=False)
+
+        kwarg_dict = {
+            'gun.k1': 600.,
+        }
+
+        self.assertRaisesRegex(NameError, 'Parameter: k1 is not found',
+                               setup._edit_input_file_schema, kwarg_dict)
 
     def tearDown(self):
         self.test_dir.cleanup()
