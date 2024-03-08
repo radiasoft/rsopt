2e009b76439729940388ffcb23121e3d0aae495a
cchall <chall@radiasoft.net>
2024-03-08 12:19:20 -0700

    Fixed bug in Genesis setup Add support for Genesis files with no lattice Add tests of Genesis parser

--- tests/test_models.py
+++ tests/test_models.py
@@ -3,6 +3,7 @@ import unittest
 from rsopt.configuration.setup.elegant import Elegant
 from rsopt.configuration.setup.opal import Opal
 from rsopt.configuration.setup.madx import Madx
+from rsopt.configuration.setup.genesis import Genesis
 
 _TMP_DIR = 'tmp'
 
@@ -212,3 +213,48 @@ class TestMadxModels(unittest.TestCase):
 
     def tearDown(self):
         self.test_dir.cleanup()
+
+
+class TestGenesisModels(unittest.TestCase):
+
+    def setUp(self):
+        self.test_dir = tempfile.TemporaryDirectory()
+        self.input_file_pegasus = 'support/genesis/genesis_pegasus.in'
+        self.input_file_tessa = 'support/genesis/genesis_tessa.in'
+
+    def test_parse_no_lattice(self):
+        Genesis.parse_input_file(self.input_file_pegasus, shifter=False)
+
+    def test_parse_with_lattice(self):
+        # lume-genesis expects lattice to be local directory, does not check input file name for a directory path
+        Genesis.parse_input_file(self.input_file_tessa, shifter=False)
+
+    def test_genesis_parameter_edit(self):
+        setup = Genesis()
+        setup.setup['input_file'] = 'not_used'
+        setup.input_file_model = Genesis.parse_input_file(self.input_file_tessa, shifter=False)
+
+        kwarg_dict = {
+            'CURPEAK': 155.0,
+            'DElGAm': 0.04,
+            'NSCZ': 0
+        }
+
+        new_model = setup._edit_input_file_schema(kwarg_dict=kwarg_dict)
+        new_model.configure_genesis(workdir=self.test_dir.name)
+
+        new_model.write_input_file()
+        new_model.write_beam()
+        new_model.write_lattice()
+
+    def test_genesis_bad_parameter(self):
+        setup = Genesis()
+        setup.setup['input_file'] = 'not_used'
+        setup.input_file_model = Genesis.parse_input_file(self.input_file_pegasus, shifter=False)
+
+        kwarg_dict = {
+            'NOTAPAR': 155.0,
+        }
+
+        self.assertRaisesRegex(ValueError, '`notapar` was not found',
+                               setup._edit_input_file_schema, kwarg_dict)
