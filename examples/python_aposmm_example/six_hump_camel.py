33682f20b342c579294a4f3fdf14a9a168da41f6
cchall <chall@radiasoft.net>
2024-12-11 15:32:58 -0500

    wip symlinks replaced with file contents

--- examples/python_aposmm_example/six_hump_camel.py
+++ examples/python_aposmm_example/six_hump_camel.py
@@ -1,1 +1,26 @@
-../../rsopt/package_data/examples/six_hump_camel.py
+
+def six_hump_camel_func(x, y):
+    """
+    Definition of the six-hump camel
+    """
+    x1 = x
+    x2 = y
+    term1 = (4-2.1*x1**2+(x1**4)/3) * x1**2
+    term2 = x1*x2
+    term3 = (-4+4*x2**2) * x2**2
+
+    return term1 + term2 + term3
+
+
+def mo_six_hump_camel_func(x, y):
+    """
+    Definition of the six-hump camel
+    """
+    x1 = x
+    x2 = y
+    term1 = (4-2.1*x1**2+(x1**4)/3) * x1**2
+    term2 = x1*x2
+    term3 = (-4+4*x2**2) * x2**2
+    # y0 = np.linalg.norm(xvals)
+
+    return term1, term2, term3
