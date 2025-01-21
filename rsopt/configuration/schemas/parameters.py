576adb7235992cf38f46782b83448198513fb47b
cchall <chall@radiasoft.net>
2025-01-21 14:36:03 -0500

    wip Parameter is usable now

--- rsopt/configuration/schemas/parameters.py
+++ rsopt/configuration/schemas/parameters.py
@@ -1,18 +1,24 @@
 from typing import Any, Optional, Union, List
 import pydantic
 
+def parameter_discriminator(v: dict) -> str:
+    if v.get('min'):
+        return 'numeric'
+    elif v.get('values'):
+        return 'category'
+
 class Parameter(pydantic.BaseModel):
     name: str
-    type: str = 'numeric'
+    group: Optional[str or int] = None
 
-class NumericParameter(pydantic.BaseModel):
+class NumericParameter(Parameter):
     # TODO: Type of all must match
     min: Union[int, float]
     max: Union[int, float]
     start: Union[int, float]
     # TODO: Checking requirement means looking at Options
     samples: Optional[int] = 1
-    scale: Optional[]
+    # scale: Optional[]
 
-class CategoryParameter(pydantic.BaseModel):
+class CategoryParameter(Parameter):
     values: List[Union[int, float, str]]
