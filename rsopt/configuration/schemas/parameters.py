405e87f865cec700fa50d24c7ec5f8567ad5d339
cchall <chall@radiasoft.net>
2025-02-13 10:08:49 -0500

    Give base parameter and setting fields to explicitly set attribute and index of the value to modify. Provides an alternative for the user if the element name has '.' in it and can be used for internal use to clean up the name parsing.

--- rsopt/configuration/schemas/parameters.py
+++ rsopt/configuration/schemas/parameters.py
@@ -16,9 +16,41 @@ def parameter_discriminator(v: dict) -> str:
     elif 'values' in v.keys():
         return 'category'
 
+class Name(pydantic.BaseModel):
+    item_name: str
+    item_property: str = ''
+    item_index: int = 0
+
 class Parameter(pydantic.BaseModel):
-    name: str
+    name: str = pydantic.Field(description='User specified name or the parameter. May include formatting to give attribute and index.')
+    item_name: str = pydantic.Field('', exclude=True, description='Internal usage. Parsed name to get just the item name.')
+    item_attribute: str = ''
+    item_index: int = 0
     group: Optional[str or int] = None
+
+    @pydantic.model_validator(mode="before")
+    @classmethod
+    def parse_name(cls, values):
+        """Try to split name into item name, item attribute, item index.
+
+        Parses rsopt's string formatting for specifying model command/element names and attributes
+        `command-or-element-name.[command-or-element-attribute].[command-index]`
+
+        If command name or element name includes '.' this formatting is not usable. As an alternative the user
+        can use the item
+
+        """
+        if 'item_attribute' in values:
+            assert 'item_index' in values, (f"Error for parameter {values['name']}. `item_index` must be set "
+                                            f"when explicitly setting `item_attribute`")
+            values['item_name'] = values['name']
+            return values
+        if 'name' in values:
+            for part, typing in zip(values['name'].split('.'), ('item_name', 'item_attribute', 'item_index')):
+                values[typing] = part
+
+        return values
+
 
 class NumericParameter(Parameter):
     # TODO: Type of all must match
