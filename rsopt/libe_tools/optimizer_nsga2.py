eab7fe42fc91287d8922400469944723e9f7b691
cchall <chall@radiasoft.net>
2025-02-16 10:23:17 -0500

    Fixes to get nsga2 generator working. Importantly adds in the persis_in field which was massing and creating a mysterious error in the allocator.

--- rsopt/libe_tools/optimizer_nsga2.py
+++ rsopt/libe_tools/optimizer_nsga2.py
@@ -5,14 +5,14 @@ from libensemble.alloc_funcs.start_only_persistent import only_persistent_gens
 
 
 # dimension for individual set at run time
-nsga2_gen_out = [('individual', float, None), ('generation', int)]
+nsga2_gen_out = [('individual', float, None), ('generation', int), ('last_points', bool)]
 _DEFAULT_WEIGHT = (-1.0, )
 
 
 class EvolutionaryOptimizer(optimizer.libEnsembleOptimizer):
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self, config_model):
+        super().__init__(config_model)
 
     def _configure_optimizer(self):
 
@@ -23,35 +23,24 @@ class EvolutionaryOptimizer(optimizer.libEnsembleOptimizer):
 
         user_keys = {'lb': list(self._config.lower_bounds),  # DEAP requires lists
                      'ub': list(self._config.upper_bounds),
+                     'indiv_size': self._config.dimension,
                      'pop_size': self._config.options.software_options.pop_size,
                      'cxpb': self._config.options.software_options.cxpb,
                      'eta': self._config.options.software_options.eta,
-                     'indpb': self._config.options.software_options.indpb}
-
-        # NSGA2 parameters
-        assert self._config.options.n_objectives > 0, "n_objectives must be set in options to a number > 0"
-
-        self._generator_defaults = {
-            # Set to DEAP implementation defaults
-            'weights': _DEFAULT_WEIGHT * self._config.options.software_options.n_objectives,
-            'cxpb': 0.9,  # probability two individuals are crossed
-            'eta': 20.0,  # large eta = low variation in children
-            'indpb': 1.0 / self._config.options.n_objectives  # Mutation probability
-        }
-
-        # If user didn't set keys contained in _generator_defaults though software_options then set them now
-        for key, val in self._generator_defaults.items():
-            user_keys.setdefault(key, val)
+                     'indpb': self._config.options.software_options.indpb,
+                     'weights': _DEFAULT_WEIGHT * self._config.options.software_options.n_objectives,
+                     }
 
         self.gen_specs.update({'gen_f': deap_nsga2,
+                               'persis_in': self._config.options.method.persis_in +
+                                            [n[0] for n in gen_out],
                                'in': ['sim_id', 'generation', 'individual', 'fitness_values'],
                                'out': gen_out,
                                'user': user_keys})
 
     def _configure_allocation(self):
         # local optimizer allocation
-        self.alloc_specs.update({'alloc_f': only_persistent_gens,
-                                 'out': [('given_back', bool)]})
+        self.alloc_specs.update({'alloc_f': only_persistent_gens, 'user': {'give_all_with_same_priority': True}})
 
     def _configure_specs(self):
         self.nworkers = self._config.options.nworkers
