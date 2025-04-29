d6bbfa0dac1669ab82fa7a78e3e98a50f5700a72
cchall <chall@radiasoft.net>
2025-04-29 10:47:23 -0400

    options module just classified imports for use by aposmm. will be removed at end of pydantic refactor

--- rsopt/configuration/options/options.py
+++ rsopt/configuration/options/options.py
@@ -1,24 +1,24 @@
-# TODO: I think this is only used in aposmm. Would be nice to remove.
-from rsopt.configuration.options import dfols
-from rsopt.configuration.options import dlib
-from rsopt.configuration.options import lh
-from rsopt.configuration.options import mesh
-from rsopt.configuration.options import nlopt
-from rsopt.configuration.options import nsga2
-from rsopt.configuration.options import aposmm
-from rsopt.configuration.options import pysot
-from rsopt.configuration.options import scipy
-from rsopt.configuration.options import mobo
-
-option_classes = {
-    'nlopt': nlopt.Nlopt,
-    'aposmm': aposmm.Aposmm,
-    'nsga2': nsga2.Nsga2,
-    'dfols': dfols.Dfols,
-    'scipy': scipy.Scipy,
-    'pysot': pysot.pySOT,
-    'dlib': dlib.Dlib,
-    'mobo': mobo.Mobo,
-    'mesh_scan': mesh.Mesh,
-    'lh_scan': lh.LH
-}
+# TODO: I think this is only used in aposmm. Should be removed.
+# from rsopt.configuration.options import dfols
+# from rsopt.configuration.options import dlib
+# from rsopt.configuration.options import lh
+# from rsopt.configuration.options import mesh
+# from rsopt.configuration.options import nlopt
+# from rsopt.configuration.options import nsga2
+# from rsopt.configuration.options import aposmm
+# from rsopt.configuration.options import pysot
+# from rsopt.configuration.options import scipy
+# from rsopt.configuration.options import mobo
+#
+# option_classes = {
+#     'nlopt': nlopt.Nlopt,
+#     'aposmm': aposmm.Aposmm,
+#     'nsga2': nsga2.Nsga2,
+#     'dfols': dfols.Dfols,
+#     'scipy': scipy.Scipy,
+#     'pysot': pysot.pySOT,
+#     'dlib': dlib.Dlib,
+#     'mobo': mobo.Mobo,
+#     'mesh_scan': mesh.Mesh,
+#     'lh_scan': lh.LH
+# }
