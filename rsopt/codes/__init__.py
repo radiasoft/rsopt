37d07ec777f93a0c5a49c6cad88d664dc333d5c3
cchall <chall@radiasoft.net>
2025-05-13 20:02:26 -0400

    register user as a setup mode

--- rsopt/codes/__init__.py
+++ rsopt/codes/__init__.py
@@ -1,6 +1,5 @@
-import pydantic
 import typing
-from rsopt.codes import spiffe, elegant, python, opal, madx, flash, genesis
+from rsopt.codes import spiffe, elegant, python, opal, madx, flash, genesis, user
 # Templated codes have schema files that can be used to check input and create run files. Otherwise user
 #   must supply module containing inputs
 TEMPLATED_CODES = ['elegant', 'opal', 'madx', 'flash']
@@ -13,4 +12,4 @@ SIREPO_SIM_TYPES = ['elegant', 'opal', 'madx', 'genesis']
 # SUPPORTED_CODES = ['python', 'user', 'genesis', *TEMPLATED_CODES]
 
 SUPPORTED_CODES = typing.Union[elegant.Elegant, flash.Flash, genesis.Genesis,
-                               madx.Madx, opal.Opal,  python.Python, spiffe.Spiffe]
+                               madx.Madx, opal.Opal,  python.Python, spiffe.Spiffe, user.User]
