dce8f646b3d80671559626553498b8f2eada891a
Rob Nagler <5495179+robnagler@users.noreply.github.com>
2025-11-21 18:00:06 -0600

    Fix #186 migrate to pyproject.toml (#187)
    - will not pass pykern ci so do not run

--- rsopt/rsopt_console.py
+++ rsopt/rsopt_console.py
@@ -1,22 +1,18 @@
-# -*- coding: utf-8 -*-
-u"""Front-end command line for :mod:`rsopt`.
+"""Front-end command line for :mod:`rsopt`.
 
 See :mod:`pykern.pkcli` for how this module is used.
 
-:copyright: Copyright (c) 2020 RadiaSoft LLC.  All Rights Reserved.
-:license: http://www.apache.org/licenses/LICENSE-2.0.html
+:copyright: Copyright (c) 2025 RadiaSoft LLC.  All Rights Reserved.
+:license: https://www.apache.org/licenses/LICENSE-2.0.html
 """
-from __future__ import absolute_import, division, print_function
-from pykern import pkcli
-from rsopt import codes
-import os
+
+import pykern.pkcli
 import sys
 
 
 def main():
-    os.environ['SIREPO_FEATURE_CONFIG_SIM_TYPES'] = ':'.join(codes.SIREPO_SIM_TYPES)
-    return pkcli.main('rsopt')
+    return pykern.pkcli.main("rsopt")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     sys.exit(main())
