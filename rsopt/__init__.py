376e7e6983e2072804b00263a080a15d7b87d766
Paul Moeller <moeller@radiasoft.net>
2025-12-16 17:32:41 +0000

    fix #188 added constants back

--- rsopt/__init__.py
+++ rsopt/__init__.py
@@ -3,6 +3,9 @@
 :copyright: Copyright (c) 2025 RadiaSoft LLC.  All Rights Reserved.
 :license: https://www.apache.org/licenses/LICENSE-2.0.html
 """
+
+from pykern import pkio
+from pykern import pkresource
 import importlib.metadata
 
 try:
@@ -10,3 +13,10 @@ try:
     __version__ = importlib.metadata.version("rsopt")
 except importlib.metadata.PackageNotFoundError:
     pass
+
+EXAMPLE_SYMLINK = pkio.py_path(pkresource.filename("examples"))
+EXAMPLE_REGISTRY = pkio.py_path(pkresource.filename("example_registry.yml"))
+EXECUTOR_SCHEMA = pkio.py_path(pkresource.filename("executor_schema.yml"))
+OPTIMIZER_SCHEMA = pkio.py_path(pkresource.filename("optimizer_schema.yml"))
+SETUP_SCHEMA = pkio.py_path(pkresource.filename("setup_schema.yml"))
+OPTIONS_SCHEMA = pkio.py_path(pkresource.filename("options_schema.yml"))
